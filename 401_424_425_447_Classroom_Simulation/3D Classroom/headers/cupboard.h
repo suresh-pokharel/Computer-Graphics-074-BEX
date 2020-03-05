36c4e2a13b2ddacd8337ff7bccaf8055edf6d43a
Aashish <Aashish@adhikari.com>
2020-03-05 22:11:49 +0545

    Classroom Simulation Added

---
+++ 401_424_425_447_Classroom_Simulation/3D Classroom/headers/cupboard.h
@@ -0,0 +1,14 @@
+#ifndef CUPBOARD_H
+#define CUPBOARD_H
+
+#include <GL/glut.h>
+#include <GL/freeglut_ext.h>
+
+class Cupboard{
+
+    public:
+		void drawCupboard();
+
+};
+
+#endif
---
+++ 401_424_425_447_Classroom_Simulation/3D Classroom/headers/shelf.h
@@ -0,0 +1,20 @@
+#ifndef SHELF_H
+#define SHELF_H
+
+#include <GL/glut.h>
+#include <GL/freeglut_ext.h>
+
+class Shelf{
+
+    public:
+		void drawSnowMan() ;
+		void drawHouse() ;
+
+		void drawCubBoard();
+
+		void drawShelf() ;
+
+
+};
+
+#endif
