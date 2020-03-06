9604ce9158e5bc0b603d3ffcd6b6301f6f3109d9
Aashish <Aashish@adhikari.com>
2020-03-06 18:46:12 +0545

    401_424_425_447_Classroom_Simulation re-added

---
+++ 401_424_425_447_Classroom_Simulation/3D Classroom/headers/window.h
@@ -0,0 +1,18 @@
+#ifndef WINDOW_H
+#define WINDOW_H
+
+#include <GL/glut.h>
+#include <GL/freeglut_ext.h>
+
+class Window{
+
+    public:
+	  void drawWindow1() ;
+
+        void drawWindow2() ;
+
+        void drawWindowSill() ;
+
+};
+
+#endif
